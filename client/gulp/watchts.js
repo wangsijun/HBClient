19deb7f417ec5b3d533398c67b17079fca4a38c6
王思俊 <wangsijun1986@hotmail.com>
2016-04-28 04:25:12 -0700

    Changed Login Page

--- client/gulp/watchts.js
+++ client/gulp/watchts.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var config = require('./config');
 
 gulp.task('watchts',function () {
-  gulp.watch(config.base + '/app/**/*.ts', ['transpile']);
+  gulp.watch(config.base + '/app/**/**/*.ts', ['transpile']);
 });
